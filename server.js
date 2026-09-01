9365b70772061fce4351ec580dd1ae5af0267f6a
tanguyenhoangminh <tanguyenhoangminh@gmail.com>
2026-09-01 09:19:07 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2/promise');
-const mqtt = require('mqtt'); Thư viện MQTT
+const mqtt = require('mqtt'); //Thư viện MQTT
 require('dotenv').config(); 
 
 // Voice NLU 
